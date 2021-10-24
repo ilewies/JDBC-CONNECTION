6ef002c61f9b6239c8178472759870f2ad73c567
Lavesh Satim <satimlavesh@gmail.com>
2021-10-25 00:25:31 +0530

    switch added for more user friendlyness

--- src/com/lavesh/main/Main.java
+++ src/com/lavesh/main/Main.java
@@ -5,18 +5,37 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.util.Scanner;
 
 import com.lavesh.DBFactory.OracleDBFactory;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		
+		System.out.println("***********************");
+		System.out.println("1. Oracle db");
+		System.out.println("2. Mysql db");
+		System.out.println("Any key for exit");
+		System.out.println("***********************");
 
-		//OracleDB.getOracleDB();
-		MysqlDB.getMysqlDB();
+		Scanner scanner = new Scanner(System.in);
+		int option = scanner.nextInt();
+
+		switch (option) {
+		case 1:
+			System.out.println(" ***oracle db*** ");
+			OracleDB.getOracleDB();
+			break;
+		case 2:
+			System.out.println(" ***mysql*** ");
+			MysqlDB.getMysqlDB();
+			break;
+		default:
+			System.out.println("...exited");
+			break;
+		}
 
 	}
-
-
 
 }
